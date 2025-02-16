cc1b823c77ab22f9415b4bdd62226be5a8a29900
Nahid44332 <nahidhossen3278@gmail.com>
2025-02-16 00:12:09 -0800

    Js Array

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let marks = [54, 34, 76, 98, 56];
+
+console.log(marks[3]); 
+
+marks[2]+marks[3];
+let sum = marks[2]+marks[3];
+
+console.log(sum);
+
+let man = {Name: "Nahid", age: 21, blood: "B+" , Contact: "+88019XXXXXXXX", Address: "Gazipur Sadar, Gazipur"};
+
+console.log(man.Name);
+console.log(man.age);
+console.log(man.Contact);
+console.log(man.Address);
+
+console.log(man.Name, man.age, man.Contact, man.Address);
